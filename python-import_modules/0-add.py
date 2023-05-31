e11cd1838c7382d98d6c7e48f5de7131a5512c04
chaher13 <chahercrbs@gmail.com>
2023-05-31 06:51:22 -0700

    Modify: file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add as add_function
 a = 1
 b = 2
-
-from add_0 import add as add_function
-
 if __name__ == "__main__":
     result = add_function(a, b)
     print("{} + {} = {}".format(a, b, result))
