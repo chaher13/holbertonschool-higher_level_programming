9ef3883f6761aad255ebcef17aab0fe9fe0977fa
chaher13 <chahercrbs@gmail.com>
2023-05-31 07:08:48 -0700

    Modify: file for second time

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-__import__ add_0
+from add_0 import add as add_function
 a = 1
 b = 2
+
 if __name__ == "__main__":
-    result = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, result), end= "\n")
+    result = add_function(a, b)
+print("{} + {} = {} \n".format(a, b, result), end="\n")
