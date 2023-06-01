d96c822b9148c935f692549c42be0338a8e3d322
chaher13 <chahercrbs@gmail.com>
2023-06-01 07:22:27 -0700

    task 2.3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,4 +5,4 @@ num_args = len(args)
 print("{} argument{}".format(num_args, 's' if num_args != 1 else ''), end='')
 print('.' if num_args == 0 else ':')
 for i, arg in enumerate(args, start=1):
-    print("{} {}".format(i, arg))
+    print("{}: {}".format(i, arg))
