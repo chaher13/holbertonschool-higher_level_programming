c9306ea0376b5179a4f1c4c4ab0fa2a4e8fdc89f
chaher13 <chahercrbs@gmail.com>
2023-06-01 07:11:27 -0700

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+from sys import argv
+args = argv[1:]
+num_args = len(args)
+print("{} argument{}".format(num_args, 's' if num_args != 1 else ''), end='')
+print('.' if num_args == 0 else ':')
+for i, arg in enumerate(args, start=1):
+    print("{} {}".format(i, arg))
