510fa596886a88c2bf802e50431c570d31f7f39f
chaher13 <chahercrbs@gmail.com>
2023-06-02 13:03:22 -0700

    Modify: purify the code to be in equal at the checker

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
-args = argv[1:]
-num_args = len(args)
-print("{} argument{}".format(num_args, 's' if num_args != 1 else ''), end='')
-print('.' if num_args == 0 else ':')
-for i, arg in enumerate(args, start=1):
-    print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+
+    if len(argv) == 1:
+        print("0 arguments.")
+
+    elif len(argv) == 2:
+
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+
+    else:
+
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
