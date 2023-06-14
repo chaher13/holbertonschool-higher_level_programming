1e63d12f8ad2257c74556e94392112eb6deceb49
chaher13 <chahercrbs@gmail.com>
2023-06-14 08:39:32 -0700

    Add: a file containing  a class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+  This class represents a square.
+
+  Attributes:
+      __size (int): The size of the square.
+
+  Methods:
+        __init__(size): Initializes a new instance of the Square class.
+"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
