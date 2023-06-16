b59e3073dded72dc51f32888c67b8f693f1b1865
chaher13 <chahercrbs@gmail.com>
2023-06-15 22:28:16 -0700

    Modify: change a syntaxe of docstring

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""
-    This class represents a square.
-
-"""
+"""This module defines the Square class."""
 
 
 class Square:
     """
     This class represents a square.
-
     Attributes:
         __size (int): The size of the square.
-
-    Methods:
-        __init__(size): Initializes a new instance of the Square class.
-        area(): Calculates the area of the square.
     """
     def __init__(self, size=0):
-
-        if type(size) is not int:
+        """
+        Initializes a new instance of the Square class.
+        Args:
+        size (int): The size of the square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
-        def area(self):
-            return self.__size ** 2
+    def area(self):
+        """
+        Calculates the area of the square
+        Returns:
+        The area of the square
+        """
+        return self.__size ** 2
