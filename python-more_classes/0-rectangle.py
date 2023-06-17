63971869a4ff756d8e0ec951b15b4643f3656991
chaher13 <chahercrbs@gmail.com>
2023-06-17 07:29:47 -0700

    Add: a README file and a file containing an empty class which initializes a Rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+""" This class stands for a rectangle """
+
+
+class Rectangle:
+    """
+    This class reprents represents an empty rectangle.
+    """
+    def __init__(self):
+        """
+        Initializes a new instance of the Rectangle class.
+        """
+        pass
