d48b0acbda0af1c6da0569aeef40363ef7faee1e
chaher13 <chahercrbs@gmail.com>
2023-06-21 23:54:48 -0700

    Add: a file containing  an empty class BaseGeometry.

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+An empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+
+    Methods:
+        __init__():
+            Initializes an instance of BaseGeometry.
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of BaseGeometry.
+        """
+        pass
