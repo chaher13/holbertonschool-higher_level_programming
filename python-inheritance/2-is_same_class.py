1e2aed62491d18a093a10e0d4f2001cab62323bc
chaher13 <chahercrbs@gmail.com>
2023-06-21 23:40:33 -0700

    Fix: the the 2 blank line before the function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,6 +4,7 @@ a function that returns True if the object is exactly
 an instance of the specified class ; otherwise False
 """
 
+
 def is_same_class(obj, a_class):
     """
     Checks if the given object belongs to the specified class.
