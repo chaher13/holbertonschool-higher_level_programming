0b8a528668bd60773a23bfee44cd1e874001482f
chaher13 <chahercrbs@gmail.com>
2023-06-23 06:58:22 -0700

    Fix: a docstring of the file

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -29,3 +29,11 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+
+        return self.__width * self.__height
+
+    def __str__(self):
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
