532866e535a8c7b4e944d238e754d0f7c5842849
chaher13 <chahercrbs@gmail.com>
2023-06-23 13:51:04 -0700

    Fix: Fast 2 with remove the docstring

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
-"""A method that creates a copy of a list"""
-
-
 def copy_list(a_list):
-    """
-    Create a copy of a list.
-
-    The function creates a new list that is a copy of the provided list.
-    It uses list slicing to create the copy,
-    which ensures that the original list remains unchanged.
-
-    Args:
-        a_list (list): The list to be copied.
-
-    Returns:
-        list: A new list that is a copy of the provided list.
-    """
     return a_list[:]
