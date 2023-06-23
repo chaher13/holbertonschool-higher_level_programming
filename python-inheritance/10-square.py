4e3a5ea4e74ffa9bd8d9baa47a259dc4b3158f28
chaher13 <chahercrbs@gmail.com>
2023-06-23 07:05:36 -0700

    Add: a file contaning a class Square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""A class Square"""
+"""Write a class BaseGeometry"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """A class representing a square.
-
-    The Square class inherits from the Rectangle class
-    and represents a square shape.
-    It inherits all the attributes and methods from the Rectangle class.
-
-    Attributes:
-        __size (int): The length of each side of the square.
-
-    Methods:
-        __init__(size): Initializes a new instance of the Square class.
-    """
-
+    """Write an class Square"""
     def __init__(self, size):
-        """Initializes a new instance of the Square class.
-
-        Args:
-            size (int): The length of each side of the square.
-        """
+        super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
