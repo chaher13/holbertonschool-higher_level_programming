33ef4c02b40dc70247b547ba95cf7c3ae1785747
chaher13 <chahercrbs@gmail.com>
2023-06-23 07:53:03 -0700

    Add:  a file containing a function that appends a string at the end of a text file (UTF8) and returns the number of characters added

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A function that writes a string to a text file (UTF8)
+and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Write text to a file.
+
+    Args:
+    filename (str): The name of the file to write to.
+    If no filename is provided, an empty string is used.
+    text (str): The text to write to the file.
+    If no text is provided, an empty string is used.
+
+    Returns:
+    None
+    """
+    with open(filename, 'a') as f:
+        f.write(text)
+    return len(text)
