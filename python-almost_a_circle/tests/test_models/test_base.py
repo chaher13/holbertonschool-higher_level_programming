d37115632b013c75dcf3bdea200da38c8d4cdab1
chaher13 <chahercrbs@gmail.com>
2023-06-29 04:35:10 -0700

    I hope it works

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Unittest for base module.
-Test cases for base class.
-"""
+
 
 import unittest
 import os
@@ -12,18 +9,14 @@ from models.square import Square
 
 
 class TestBaseClass(unittest.TestCase):
-    """A test class created to run tests for Base class"""
-
     def setUp(self):
         Base._Base__nb_objects = 0
 
     def test_0_id_None(self):
-        '''Test for id with None argument passed'''
         b1 = Base(None)
         self.assertEqual(b1.id, 1)
 
     def test_1_id(self):
-        '''Test to check for id'''
         b1 = Base()
         b2 = Base()
         b3 = Base(11)
@@ -40,34 +33,64 @@ class TestBaseClass(unittest.TestCase):
         self.assertEqual(b7.id, 3)
 
     def test_2_id_single(self):
-        '''Test for single instance creation with id'''
         b1 = Base(3)
         self.assertEqual(b1.id, 3)
 
     def test_3_id_multi(self):
-        '''Test for multiple instance creation with id'''
         b1 = Base(3)
         self.assertEqual(b1.id, 3)
         b2 = Base(4)
         self.assertEqual(b2.id, 4)
 
     def test_4_id_string(self):
-        '''Test for string argument'''
         b1 = Base("foo")
         self.assertEqual(b1.id, "foo")
 
     def test_5_id_NaN(self):
-        '''Test for NaN as argument'''
         b1 = Base(float("nan"))
         self.assertNotEqual(b1.id, float("nan"))
 
     def test_6_id_sameId(self):
-        '''Test for same ids'''
         b1 = Base(22)
         self.assertEqual(b1.id, 22)
         b2 = Base(22)
         self.assertEqual(b2.id, 22)
 
+    def test_7_to_json_string(self):
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
 
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+        json_string = Base.to_json_string([{"id": 1, "name": "foo"},
+                                           {"id": 2, "name": "bar"}])
+        self.assertEqual(json_string, '[{"id": 1, "name": "foo"},\
+ {"id": 2, "name": "bar"}]')
+
+    def test_8_save_to_file(self):
+        Rectangle.save_to_file([])
+        self.assertTrue(os.path.isfile("Rectangle.json"))
+        with open("Rectangle.json", "r") as file:
+            content = file.read()
+            self.assertEqual(content, "[]")
+
+        Square.save_to_file(None)
+        self.assertTrue(os.path.isfile("Square.json"))
+        with open("Square.json", "r") as file:
+            content = file.read()
+            self.assertEqual(content, "[]")
+
+    def test_9_from_json_string(self):
+        json_string = '[]'
+        result = Base.from_json_string(json_string)
+        self.assertEqual(result, [])
+
+        json_string = '[{"id": 1, "name": "foo"}, {"id": 2, "name": "bar"}]'
+        result = Base.from_json_string(json_string)
+        self.assertEqual(result, [{"id": 1, "name": "foo"},
+                                  {"id": 2, "name": "bar"}])
+
+        
 if __name__ == '__main__':
-    unittest.main()
+        unittest.main()
