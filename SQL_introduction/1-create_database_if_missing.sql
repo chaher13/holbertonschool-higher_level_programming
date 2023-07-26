a0f857bed9aacf3875682d8baf51ca62b8eb8de4
chaher13 <chahercrbs@gmail.com>
2023-07-25 19:27:06 -0700

    Add:  a script that creates the database hbtn_0c_0 in my MySQL server, If the database hbtn_0c_0 already exists, my script should not fail, I'm not allowed to use the SELECT or SHOW statements

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- This script creates the database "hbtn_0c_0" if it doesn't already exist.
+-- Use the IF NOT EXISTS clause to prevent failure if the database already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
