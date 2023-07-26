8524eec22f632876c67491ea8af128a9b8eb5b79
chaher13 <chahercrbs@gmail.com>
2023-07-25 19:56:59 -0700

    Add:  a script that prints the full description of the table first_table from the database hbtn_0c_0 in my MySQL server

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- This script retrieves the complete table definition of "first_table"-- The "SHOW CREATE" TABLE command displays the SQL statement used to create the table
+SHOW CREATE TABLE first_table;
