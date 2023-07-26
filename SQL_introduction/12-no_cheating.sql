44dd76c69a1e506658b664ee884f1ecaedafcfd9
chaher13 <chahercrbs@gmail.com>
2023-07-25 20:39:52 -0700

    Add:  a script that updates the score of Bob to 10 in the table second_table, I'm not allowed to use Bob’s id value, only the name field

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Script to update the score of the record with name='Bob' to 10 in the table "second_table".
+-- The query utilizes the UPDATE statement to modify the score field.
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
