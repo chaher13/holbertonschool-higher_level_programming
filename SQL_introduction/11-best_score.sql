3e0339248427ca1a370aee3cb36034f71d5ef3a5
chaher13 <chahercrbs@gmail.com>
2023-07-25 20:32:47 -0700

    Add:  a script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in my MySQL server, Results should display both the score and the name (in this order), Records should be ordered by score (top first)

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
 -- This script lists all records with a score >= 10 in the table "second_table" in the "hbtn_0c_0" database on your MySQL server.
 -- Results display both the score and the name, ordered by score in descending order (top score first).
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
