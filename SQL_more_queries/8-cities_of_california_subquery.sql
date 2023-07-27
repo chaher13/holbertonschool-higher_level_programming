7ef429b7e15ef160b14987982da73a27e8294514
chaher13 <chahercrbs@gmail.com>
2023-07-26 19:18:16 -0700

    Add: change the select info

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Script to list all the cities of California found in the database hbtn_0d_usa using a subquery.
 -- The subquery retrieves the id of the state where name = California.
-SELECT *
+SELECT id, name
 FROM cities
 WHERE state_id = (
     SELECT id
