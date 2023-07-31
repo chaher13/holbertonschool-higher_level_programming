eb2d98e5859e3ee839e1494a774b64f5d14a0bd4
chaher13 <chahercrbs@gmail.com>
2023-07-31 20:00:23 +0200

    update: to correct an error syntax in the query part

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,24 +4,24 @@
 This is a script  that lists all states from the database hbtn_0e_0_usa
 """
 
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
 
-from sys import argv
-import MySQLdb
 
+    db  = MySQLdb.connect(host='localhost',
+                        port=3306,
+                        user=argv[1],
+                        passwd=argv[2],
+                        db=argv[3])
 
-db  = MySQLdb.connect(host='localhost',
-                      port=3306,
-                      user=argv[1],
-                      passwd=argv[2],
-                      db=argv[3])
+    cursor = db.cursor()
 
-cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-cursor.execute("SELECT * FROM states ORBER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
