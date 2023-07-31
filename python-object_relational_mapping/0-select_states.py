8c04bd0d13d763ea38cb241557fdb32a03fa0bb8
chaher13 <chahercrbs@gmail.com>
2023-07-31 20:11:25 +0200

    update: pecodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,12 +9,11 @@ if __name__ == '__main__':
     from sys import argv
     import MySQLdb
 
-
-    db  = MySQLdb.connect(host='localhost',
-                        port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        db=argv[3])
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
     cursor = db.cursor()
 
