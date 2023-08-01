5d1faecc6edc9a3eb4f31a84aff8b6f62973fb71
chaher13 <chahercrbs@gmail.com>
2023-08-02 00:19:56 +0200

    Add:  a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).filter(State.name.ilike('%a%'))\
-        .order_by(State.id).all()
+            .order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
