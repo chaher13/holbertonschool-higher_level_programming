6760d98287274cf847c396bd94f2b339e1fcd8ed
chaher13 <chahercrbs@gmail.com>
2023-08-01 11:57:45 +0200

    update 3 task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -19,12 +19,13 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    queries = "SELECT cities.name\
+    queries = "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')\
             FROM cities JOIN states ON cities.state_id = states.id\
             WHERE states.name = %s ORDER BY cities.id ASC"
     name = argv[4]
     cursor.execute(queries, (name, ))
     rows = cursor.fetchall()
-    print(", ".join(row[0] for row in rows))
+    for row in rows:
+        print(row)
     cursor.close()
     db.close()
