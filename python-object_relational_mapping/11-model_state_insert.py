77c361e89841a2e8ee4ba35d18ef833fe51d21a8
chaher13 <chahercrbs@gmail.com>
2023-08-02 00:18:29 +0200

    Add:  a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+This is   a script that adds the State object “Louisiana”
+to the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisana")
+    session.add(new_state)
+    session.commit()
+
+    insert_states = session.query(State).filter(State.name == "Louisana").one()
+    print(insert_states.id)
+
+    session.close()
